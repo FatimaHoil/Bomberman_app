c65ed0de049f52571d466fb11326254370573b38
FatimaHoil <LE23080267@merida.tecnm.mx>
2024-11-19 14:29:40 -0600

    Bonberman_inicio

---
+++ Bomberman/lib/homepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+class HomePages extends StatefulWidget {
+
+  @override
+  State<HomePages> createState() => _HomePagesState();
+}
+
+class _HomePagesState extends State<HomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
